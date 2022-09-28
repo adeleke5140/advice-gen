22c39cb703bd81fcc804471ef832b02d60184b18
adeleke5140 <adelekekehinde06@gmail.com>
2022-09-28 01:16:29 +0100

    :rocket: add in all the relevant files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AdviceCard from './components/AdviceCard.vue';
+
+</script>
+
+<template>
+  <span class="text-light-cyan text-xs mb-8 block">Click the dice to get random advice!</span>
+  <AdviceCard/>
+  <div class="attribution text-light-cyan absolute bottom-0">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">adeleke5140</a>.
+  </div>
+</template>
+
+<style>
+
+
+</style>
---
+++ src/components/AdviceCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="relative rounded-lg mb-8 p-4 bg-dark-grayish-blue w-80 h-[19rem] text-light-cyan flex flex-col justify-center items-center">
+        <p class="text-xs text-neon-green font-normal uppercase tracking-widest">Advice #{{ adviceNo }}</p>
+
+        <p class="mt-4 text-xl pt-2 p-3 hover:shadow-neon-green hover:shadow-lg">
+           "{{ advice }}"
+        </p>
+
+        <svg class="" width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+
+        <button @click="fetchAdvice" class="bg-neon-green hover:shadow-neon-green p-4 rounded-full absolute bottom-[-1.5rem]">
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+        </button>
+       
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const adviceNo = ref(null)
+const advice = ref(null)
+
+ function fetchAdvice(){
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => data.slip)
+        .then(output => {
+            adviceNo.value = output.id
+            advice.value = output.advice
+        })
+}
+
+
+onMounted(() => fetchAdvice())
+
+</script>
+
+<style scoped>
+
+</style>
